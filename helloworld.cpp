3b54c8a64363572e11baf1ee4619eb6a088dc5a6
pickerj <pickerj@live.com>
2016-11-04 16:37:38 -0400

    Added helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+/* James Picker
+ * 4th November 2016
+ * helloworld.cpp
+ * A simple "Hello World" program
+ * (for testing purposes)
+ */
+
+#include <iostream>
+
+// Print "Hello, world!" to the screen
+int main()
+{
+  cout << "Hello, world!\n";
+}
